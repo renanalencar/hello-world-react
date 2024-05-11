b91b4189fb95129acf10701bb4457a45422ef346
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-05-11 10:36:30 -0300

    Cria o primeiro componente do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edite <code>src/App.js</code> e salve para recarregar.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Aprenda React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// App.js
+import React from "react";
+import MeuComponente from "./components/MeuComponente";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React
-        </a>
-      </header>
+    <div>
+      <h1>Meu Primeiro App React</h1>
+      <MeuComponente nome="Mundo" />
     </div>
   );
 }
---
+++ src/components/MeuComponente.js
@@ -0,0 +1,8 @@
+// MeuComponente.js
+import React from 'react';
+
+function MeuComponente(props) {
+  return <div>Olá, {props.nome}!</div>;
+}
+
+export default MeuComponente;
